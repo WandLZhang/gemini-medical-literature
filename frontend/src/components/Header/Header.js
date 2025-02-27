86c68e67f4dfbbd77529f63d89145767fb6f0d78
WandLZhang <willis.zhng@gmail.com>
2025-02-27 11:38:35 -0500

    change header color

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,12 +4,12 @@ import UserMenu from './UserMenu';
 
 const Header = ({ user, handleLogin, handleLogout, showUserMenu, setShowUserMenu, isAuthenticated }) => {
   return (
-    <header className="h-16 p-4 flex justify-end items-center bg-modernOrange-700 text-white">
+    <header className="h-16 p-4 flex justify-end items-center bg-[#FF7F00] text-white">
       <div className="flex items-center">
         {!isAuthenticated && (
           <button
             onClick={handleLogin}
-            className="mr-4 px-4 py-2 bg-white text-modernOrange-700 rounded hover:bg-modernOrange-100 transition-colors"
+            className="mr-4 px-4 py-2 bg-white text-[#FF7F00] rounded hover:bg-orange-100 transition-colors"
           >
             Login
           </button>
