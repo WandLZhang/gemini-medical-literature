de60af8363f7775899577f0da104f3097e0916be
WandLZhang <willis.zhng@gmail.com>
2025-03-09 01:23:12 -0500

    loading spinner debug

--- frontend/src/components/MainPanel/AnalysisSection.js
+++ frontend/src/components/MainPanel/AnalysisSection.js
@@ -69,7 +69,7 @@ useEffect(() => {
               <svg className="w-4 h-4 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
               </svg>
-            ) : (isRetrieving || !showProcessingMessage) ? (
+            ) : (!isProcessingArticles && isRetrieving) ? (
               <LoadingSpinner className="h-4 w-4" />
             ) : (
               <svg className="w-4 h-4 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
